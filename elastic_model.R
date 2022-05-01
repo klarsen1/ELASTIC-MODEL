edf1e21f4fcab02261f04cbf6633995a11b250a0
Kim Larsen <kim.larsen@rtd.lan>
2022-05-01 08:20:45 -0700

    Add home court advantage

--- elastic_model.R
+++ elastic_model.R
@@ -22,7 +22,8 @@ box_df <-
   separate(fg3, into=c("fg3_made", "fg3_attempts"), sep="-") %>%
   separate(fg, into=c("fg_made", "fg_attempts"), sep="-") %>%
   separate(ft, into=c("ft_made", "ft_attempts"), sep="-") %>%
-  mutate(season=as.numeric(season),
+  mutate(row=row_number(),
+         season=as.numeric(season),
          season_type=as.numeric(season_type),
          game_date=as.Date(game_date)-1,
          game_id=as.numeric(game_id),
@@ -47,15 +48,20 @@ box_df <-
          attempts=(ft_attempts+fg_attempts+fg3_attempts)/s,
          plus_minus_n=if_else(grepl("+", plus_minus), as.numeric(gsub("+", "", plus_minus)), -as.numeric(gsub("-", "", plus_minus)))) %>%
   filter(!(team_abbreviation %in% c("LEB", "USA", "WORLD", "GIA", "DUR"))) %>%
+  group_by(game_id) %>%
+  mutate(home=if_else(row==max(row), 1, 0)) %>%
   group_by(game_id, team_id) %>%
-  mutate(team_pts=sum(pts), 
+  mutate(home=max(home),
+         team_pts=sum(pts), 
          share_min=min/sum(min), 
          current_playoffs=if_else(season==current_year & season_type==3, 1, 0)) %>%
   group_by(athlete_id) %>%
   mutate(games_played_ever=sum(played_min)) %>%
   arrange(game_date, game_id, team_id) %>%
-  dplyr::select(games_played_ever, current_playoffs, season, season_type, athlete_id, athlete_display_name, team_id, team_abbreviation, game_id, game_date, team_pts, oreb, attempts, dreb, stl, ast, min, blk, to, pts, ft_made, ft_attempts, fg_made, fg_attempts, fg3_made, fg3_attempts, share_min, plus_minus_n) %>%
-  ungroup()
+  dplyr::select(home, games_played_ever, current_playoffs, season, season_type, athlete_id, athlete_display_name, team_id, team_abbreviation, game_id, game_date, team_pts, oreb, attempts, dreb, stl, ast, min, blk, to, pts, ft_made, ft_attempts, fg_made, fg_attempts, fg3_made, fg3_attempts, share_min, plus_minus_n) %>%
+  ungroup() %>%
+  filter(!between(game_date, as.Date("2020-07-09"), as.Date("2020-10-11")))
+  
 
 
 ################### Generate clusters
@@ -116,6 +122,7 @@ clusters$athlete_id <- as.numeric(clusters$athlete_id)
 dist <- group_by(clusters, cluster) %>%
   summarise(n=n())
 
+set.seed(2022)
 ################### Set up data for modeling by calculating the net differences in cluster distributions
 box_df_clusters <- left_join(box_df, dplyr::select(clusters, -athlete_display_name), by=c("athlete_id", "season")) %>%
   replace_na(list(cluster=0)) %>%
@@ -123,13 +130,13 @@ box_df_clusters <- left_join(box_df, dplyr::select(clusters, -athlete_display_na
   ## This data will not be used for modeling so it's OK
   ## Need to change this to create the injury adjusted percentages from the player data
   filter(!(current_playoffs==1 & athlete_display_name=="Joel Embiid")) %>% 
-  group_by(current_playoffs, season, season_type, game_id, game_date, team_abbreviation, team_id, team_pts, cluster) %>%
+  group_by(current_playoffs, season, season_type, game_id, game_date, team_abbreviation, team_id, team_pts, cluster, home) %>%
   summarise(min=sum(min)) %>%
   arrange(game_id, team_pts) %>%
   group_by(game_id) %>%
   mutate(win=if_else(dense_rank(team_pts)==2, 1, 0)) %>%
   ungroup() %>%
-  pivot_wider(id_cols=c("game_id", "team_id", "game_date", "team_abbreviation", "win", "team_pts", "season", "season_type", "current_playoffs"), names_from = cluster, values_from = min, names_prefix = "min_", values_fill = 0) %>%
+  pivot_wider(id_cols=c("game_id", "team_id", "game_date", "team_abbreviation", "win", "team_pts", "season", "season_type", "current_playoffs", "home"), names_from = cluster, values_from = min, names_prefix = "min_", values_fill = 0) %>%
   group_by(game_id) %>%
   mutate(r = sample(100,1), 
          team_indicator=case_when(r<50 & row_number()==1 ~ 2, 
@@ -140,26 +147,27 @@ box_df_clusters <- left_join(box_df, dplyr::select(clusters, -athlete_display_na
   arrange(game_id, team_indicator) %>%
   mutate(total_min=rowSums(across(starts_with("min"))), 
          m=if_else(team_indicator==1, 1, -1), 
-         label=if_else(team_indicator==1 & win==1, 1, 0)) %>% 
+         label=if_else(team_indicator==1 & win==1, 1, 0), 
+         home_team=if_else(team_indicator==1 & home==1, 1, 0)) %>% 
   mutate_at(vars(starts_with("min")), ~(. / total_min*m)) %>%
   arrange(game_date, game_id, team_id) %>%
   ungroup()
 
 box_df_clusters_agg <-
   group_by(box_df_clusters, game_id, season, season_type, current_playoffs) %>%
-  summarise_at(vars(starts_with("min"), label), sum) %>%
+  summarise_at(vars(starts_with("min"), label, home_team), sum) %>%
   ungroup()
 
 ################### Fit the model (excluding the modified 2022 playoff data for model estimation)
-t <- filter(box_df_clusters_agg, current_playoffs==0)
+t <- filter(box_df_clusters_agg)
 train <- sample_frac(t, .7) 
 valid <- filter(t, !(game_id %in% train$game_id))
 rm(t)
 
 Y <- train$label
 Y_VALID <- valid$label
-X <- model.matrix(as.formula(Y ~ .), dplyr::select(train, starts_with("min")))
-X_VALID <- model.matrix(as.formula(Y_VALID ~ .), dplyr::select(valid, starts_with("min")))
+X <- model.matrix(as.formula(Y ~ .), dplyr::select(train, starts_with("min"), home_team))
+X_VALID <- model.matrix(as.formula(Y_VALID ~ .), dplyr::select(valid, starts_with("min"), home_team))
 
 
 model <- cv.glmnet(y=Y, x=X, family="binomial", parallel=FALSE, nfolds=10)
@@ -174,35 +182,62 @@ auc(roc(Y, p))
 
 ################### Run the playoffs
 ### Get the most recent cluster distributions from the modified playoff data
-playoffs <- filter(box_df_clusters, current_playoffs==1)
+playoffs <- 
+  left_join(box_df, dplyr::select(clusters, -athlete_display_name), by=c("athlete_id", "season")) %>%
+  replace_na(list(cluster=0)) %>%
+  filter(athlete_display_name != "Joel Embiid") %>% # injured
+  filter(athlete_display_name != "Khris Middleton") %>% # injured
+  group_by(game_id, game_date, team_abbreviation, team_id, cluster, current_playoffs) %>%
+  summarise(min=sum(min)) %>%
+  ungroup() %>%
+  pivot_wider(id_cols=c("game_id", "team_id", "game_date", "team_abbreviation", "current_playoffs"), names_from = cluster, values_from = min, names_prefix = "min_", values_fill = 0) %>%
+  mutate(total_min=rowSums(across(starts_with("min")))) %>% 
+  mutate_at(vars(starts_with("min")), ~(. / total_min)) %>%
+  arrange(game_date, game_id, team_id) %>%
+  filter(current_playoffs==1) %>%
+  dplyr::select(team_abbreviation, starts_with("min")) %>%
+  ungroup()
 
 ### Helper function to get the probability from the elastic net model and simulate the 7 game series
 matchup <- function(team1, team2){
   team1_stats <- filter(playoffs, team_abbreviation==team1)  %>%
     summarise(across(starts_with("min"), ~ mean(abs(.x), na.rm = TRUE))) %>%
     mutate(total_min=rowSums(across(starts_with("min")))) %>%
-    mutate_at(vars(starts_with("min")), ~(. / total_min)) 
+    mutate_at(vars(starts_with("min")), ~(. / total_min)) %>%
+    mutate(total_min=rowSums(across(starts_with("min"))))
 
+  #print(team1_stats$total_min)
+  
   team2_stats <- filter(playoffs, team_abbreviation==team2)  %>%
     summarise(across(starts_with("min"), ~ mean(abs(.x), na.rm = TRUE))) %>%
     mutate(total_min=rowSums(across(starts_with("min")))) %>%
     mutate_at(vars(starts_with("min")), ~(. / -total_min)) %>%
-    dplyr::select(-total_min)
+    mutate(total_min=rowSums(across(starts_with("min"))))
+  
+  #print(team2_stats$total_min)
 
-  input <- bind_rows(team1_stats, team2_stats) %>%
+  input1 <- bind_rows(team1_stats, team2_stats) %>%
     summarise_at(vars(starts_with("min")), sum) %>%
+    mutate(home_team=1) %>%
     ungroup()
+
+  input2 <- 
+    mutate(input1, home_team=0) 
   
   Y <- 0
-  X <- model.matrix(as.formula(Y ~ .), dplyr::select(input, starts_with("min")))
-  p <- as.numeric(1/(1+exp(-X%*%c[-1])))
+  X1 <- model.matrix(as.formula(Y ~ .), dplyr::select(input1, starts_with("min"), home_team))
+  p1 <- as.numeric(1/(1+exp(-X1%*%c[-1])))
+  X2 <- model.matrix(as.formula(Y ~ .), dplyr::select(input2, starts_with("min"), home_team))
+  p2 <- as.numeric(1/(1+exp(-X2%*%c[-1])))
+  p <- c(p1, p1, p2, p2, p1, p2, p1)
+  
   n1 <- 0
   n2 <- 0
   for (k in 1:25000){
     nn1 <- 0
     nn2 <- 0
     for (g in 1:7){
-      binomial <- as.numeric(rbinom(n=1, size=1, prob=p))  
+      binomial <- as.numeric(rbinom(n=1, size=1, prob=p[g]))  
       if (binomial==1){nn1 <- nn1+1}
       else {nn2 <- nn2+1}
     }
@@ -218,8 +253,8 @@ matchup <- function(team1, team2){
 
 ### Run the games for round 2
 
-matchup("GS", "MEM")
+matchup("MEM", "GS")
 matchup("PHX", "DAL")
 
 matchup("MIA", "PHI")
-matchup("MIL", "BOS") 
+matchup("BOS", "MIL") 
